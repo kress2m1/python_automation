59443b679e30b1f3f1f4671a393e03633f635eb2
Yetty.onaeko <yetty2010@hotmail.com>
2017-02-05 21:14:23 +0000

    added all python file

---
+++ FirstClass/Boolean/BoolSamples.py
@@ -0,0 +1,24 @@
+a = True
+print(a)
+print("'a' is: " + str(a))
+b = False
+print("'b' is: " + str(b))
+print(b)
+print("***************")
+print("Any number apart from 0 has a boolean value of 'True'")
+print("The boolean of '0' is: " + str(bool(0)))
+print("The boolean of 1 is: " + str(bool(1)))
+print("The boolean of -1 is: " + str(bool(-1)))
+c = ""
+print("The boolean of an empty string is " + str(bool(c)))
+c = " "
+print("The boolean of a non-empty string is: " + str(bool(c)))
+print('*********************************')
+
+a = "this boolean lang 'checking' the information"
+b = "This info give insight about single 'quotes' check"
+print(a)
+print(b)
+
+c = "Another way to handle \"quote\""
+print(c)
---
+++ FirstClass/Stringsample/StringFormatting.py
@@ -0,0 +1,13 @@
+
+# using values in multiple places
+#Declare and define the variable
+bag = "Channel"
+shoe = "christian louboutin"
+number = 4
+print("My Bag is " + bag + " and my shoe is " + shoe)
+
+print("My bag is %s and my shoe is %s and i live at nos %s" %(shoe, bag, number))
+print("My bag is %s" % bag)
+
+x='abc'[-3]
+print(x)
